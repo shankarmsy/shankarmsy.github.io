6671b07ee093be48856dcf3fb9cdde000835bc26
shankarmsy <Shankar.Muthuswamy@gmail.com>
2014-11-25 01:00:36 -0800

    Update

--- conf.py
+++ conf.py
@@ -648,6 +648,7 @@ COMMENT_SYSTEM_ID = "shankarmsy"
 # Social buttons. This is sample code for AddThis (which was the default for a
 # long time). Insert anything you want here, or even make it empty.
 # (translatable)
+SOCIAL_BUTTONS_CODE = ""
 # SOCIAL_BUTTONS_CODE = """
 # <!-- Social buttons -->
 # <div id="addthisbox" class="addthis_toolbox addthis_peekaboo_style addthis_default_style addthis_label_style addthis_32x32_style">
