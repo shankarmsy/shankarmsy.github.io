d69564a02868055f66d57a42255d27d00c2086c6
shankarmsy <Shankar.Muthuswamy@gmail.com>
2015-03-03 16:21:39 -0800

    Updates

--- conf.py
+++ conf.py
@@ -114,9 +114,10 @@ NAVIGATION_LINKS = {
         ("/archive.html", "Archive"),
         ("/categories/index.html", "Tags"),
         # ('/stories/projects.html', 'Projects'),
-        ((("/stories/intro.html", "Goal of the Blog"),
+        ("/stories/intro.html", "About"),
+        # ((("/stories/intro.html", "Goal of the Blog"),
         # ("/stories/background.html", "My Background")), "About"),
-        ("/posts/why-data-scientist.html", "Why Data Science")), "About"),
+        # ("/posts/why-data-scientist.html", "Why Data Science")), "About"),
         # ("/rss.xml", "RSS feed"),
     ),
 }
@@ -685,7 +686,7 @@ SHOW_SOURCELINK = False
 
 # Modify the number of Post per Index Page
 # Defaults to 10
-INDEX_DISPLAY_POST_COUNT = 5
+INDEX_DISPLAY_POST_COUNT = 4
 
 # By default, Nikola generates RSS files for the website and for tags, and
 # links to it.  Set this to False to disable everything RSS-related.
